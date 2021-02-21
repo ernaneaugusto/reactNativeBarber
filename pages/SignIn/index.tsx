8bbd498a9fb162d0e54a4523ac452cf7dc156434
Ernane Augusto <ernaneatcipriano@gmail.com>
2021-02-21 15:14:55 -0300

    finalizado front tela SignIn e add lib helper para iphone

--- pages/SignIn/index.tsx
+++ pages/SignIn/index.tsx
@@ -1,22 +1,79 @@
 import React from "react";
-import { Image } from "react-native";
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 import Button from "./../../src/components/Button";
 import Input from "./../../src/components/Input";
 import logoImg from "./../../src/assets/logo.png";
-import { Container, Title } from "./styles";
+import Icon from "react-native-vector-icons/Feather";
+import AppStyles from "./../../config/styles";
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountText,
+} from "./styles";
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
-      <Title>Fazer login</Title>
+    <>
+      {/**
+       * KeyboardAvoidingView: View que tenta se adequar
+       * ao abrir o teclado para que nao fique sobre o input
+       * que o usuario esta digitando
+       *
+       * enabled: Define que estara habilitado por padrao
+       *   */}
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+        enabled
+      >
 
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="passowrd" icon="lock" placeholder="Senha" />
-      <Button
-        onPress={() => console.log("Clicou")}
-      >Entrar</Button>
-    </Container>
+        {/**
+             * ScrollView: Tera a funcao de permitir o scroll da tela
+             * com o teclado aberto
+             * 
+             * keyboardShouldPersistTaps: Adiciona o comportamento padrao
+             * do sistema para acao fechar ou nao de clicar fora do teclado
+             * */}
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{flex: 1}}
+        >
+          <Container>
+            <Image source={logoImg} />
+            {/**
+             * Essa View em volta de um componente Text faz com que,
+             * no iOS, o Text tenha um efeito de transicao suave, pois
+             * elementos Text nao refletem esse comportamento no iOS
+             * */}
+            <View>
+              <Title>Fazer login</Title>
+            </View>
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="passowrd" icon="lock" placeholder="Senha" />
+            <Button onPress={() => console.log("Clicou")}>Entrar</Button>
+
+            <ForgotPassword>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton activeOpacity={0.7}>
+        <Icon name="log-in" size={20} color={AppStyles.orange} />
+        <CreateAccountText>Criar uma conta</CreateAccountText>
+      </CreateAccountButton>
+    </>
   );
 };
 
--- pages/SignIn/styles.ts
+++ pages/SignIn/styles.ts
@@ -1,16 +1,48 @@
 import styled from "styled-components/native";
-import MyStyles from "./../../config/styles";
+import { getBottomSpace } from "react-native-iphone-x-helper";
+import AppStyles from "./../../config/styles";
 
 export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  padding: 0 ${MyStyles.paddingDefault};
+  padding: 0 ${AppStyles.paddingDefault};
 `;
 
 export const Title = styled.Text`
   font-size: 24px;
-  color: ${MyStyles.grayLight};
-  font-family: ${MyStyles.fonts.robotoMedium};
+  color: ${AppStyles.grayLight};
+  font-family: ${AppStyles.fonts.robotoMedium};
   margin: 64px 0 24px;
+`;
+
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+export const ForgotPasswordText = styled.Text`
+  color: ${AppStyles.grayLight};
+  font-size: 16px;
+  font-family: ${AppStyles.fonts.robotoRegular};
+`;
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: ${AppStyles.grayDark};
+  border-top-width: 1px;
+  border-color: ${AppStyles.grayExtremeDark};
+  padding: 16px 0 ${10 + getBottomSpace()}px;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const CreateAccountText = styled.Text`
+  color: ${AppStyles.orange};
+  font-size: 18px;
+  font-family: ${AppStyles.fonts.robotoRegular};
+  margin-left: 16px;
 `;
