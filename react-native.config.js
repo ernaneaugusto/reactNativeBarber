2a6b8985091d8c1aa364a96b6e6e60262fce3156
Ernane Augusto <ernaneatcipriano@gmail.com>
2021-02-21 12:00:43 -0300

    instalacao de fontes externas e link com pastas nativas

---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+// executar o comando no terminal para
+// linkar os estilos nas pastas do
+// android e ios: npx react-native link
+module.exports = {
+  project: {
+    android: {},
+    ios: {},
+  },
+  assets: ["./assets/fonts/"],
+};
